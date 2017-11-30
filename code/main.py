2fdeefbf5a08bd158bc81b88e29b1dc2d90c213d
Brennen Bouwmeester <brennen.bouwmeester@gmail.com>
2017-11-30 14:47:38 +0100

    Updated_DB

--- code/main.py
+++ code/main.py
@@ -7,12 +7,12 @@ import numpy as np
 from sklearn.svm import LinearSVC
 from sklearn import svm
 
-
+y = process_output()
 # Read in the data of the match and of the players
 x2 = read_data("Match", "home_player_1")[:1000]
-p1_rating = stats_from_id(x2, "overall_rating", [70])
+p1_rating = stats_from_id(x2, "overall_rating", [100])
 
-y = process_output()
+print(p1_rating)
 
 # split into training and test
 x_train = p1_rating[:800]
@@ -21,18 +21,18 @@ x_test = p1_rating[800:1000]
 y_test = y[800:1000]
 
 # Apply linear regression models [MODELS ARE VERY BAD ATM]
-algorithm_parameters = "solver='lbfgs', multi_class='multinomial'"
-clf = OneVsRestClassifier(LinearSVC())
-result1=clf.fit(x_train, y_train).predict(x_test)
+#algorithm_parameters = "solver='lbfgs', multi_class='multinomial'"
+#clf = OneVsRestClassifier(LinearSVC())
+#result1=clf.fit(x_train, y_train).predict(x_test)
 
-otherclf= LogisticRegression()
-result2 = otherclf.fit(x_train,y_train).predict(x_test)
+#otherclf= LogisticRegression()
+#result2 = otherclf.fit(x_train,y_train).predict(x_test)
 
 # print(x_train)
 # clf3 = svm.SVC()
 # clf3p = clf3.fit(x_train, y_train)
 # print(clf3p.predict(x_test))
 # print(x_test[-7:])
-print(result1)
-print(result2)
+#print(result1)
+#print(result2)
 
