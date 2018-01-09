38270359744686e72fcc325497038064295835a1
WIllem Verhoef <willem-verhoef@Hotmail.com>
2018-01-09 09:08:43 +0100

    no message

--- code/Splitdata.py
+++ code/Splitdata.py
@@ -18,7 +18,7 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
@@ -34,7 +34,8 @@ from scipy import interp
 #                             "A_buildUpPlaySpeed, A_buildUpPlayPassing, A_chanceCreationPassing, A_chanceCreationCrossing, A_chanceCreationShooting, A_defencePressure, A_defenceAggression, A_defenceTeamWidth
 x2 = read_data("Match_DEF", "H_rating1, H_rating2, H_rating3, H_rating4, H_rating5, H_rating6, H_rating7, H_rating8, H_rating9, H_rating10, H_rating11, "
                             "A_rating1, A_rating2, A_rating3, A_rating4, A_rating5, A_rating6, A_rating7, A_rating8, A_rating9, A_rating10, A_rating11, "
-                            "H_chanceCreationCrossing, H_defencePressure, H_defenceAggression, A_chanceCreationCrossing, A_defencePressure, A_defenceAggression, "
+                            "H_buildUpPlaySpeed, H_buildUpPlayPassing, H_chanceCreationPassing, H_chanceCreationCrossing, H_chanceCreationShooting, H_defencePressure, H_defenceAggression, H_defenceTeamWidth, "
+                            "A_buildUpPlaySpeed, A_buildUpPlayPassing, A_chanceCreationPassing, A_chanceCreationCrossing, A_chanceCreationShooting, A_defencePressure, A_defenceAggression, A_defenceTeamWidth, "
                             "B365H, B365D, B365A")
 
 # x2 = read_data("Match_DEF", "B365H, B365D, B365A, BWH, BWD, BWA, IWH, IWD, IWA, LBH, LBD, LBA")
@@ -54,10 +55,10 @@ for i in range(21000):
         y1.append(y[i])
         odds.append((x2[i][-3:]))
 print(len(x))
-
+print(x)
 
 # splits the data into training and test sets
-x_train, x_test, y_train, y_test = train_test_split(x, y1, test_size=0.3, shuffle=True )
+x_train, x_test, y_train, y_test = train_test_split(x, y1, test_size=0.2, shuffle=True )
 
 #analyse the classes distribution
 length = len(y1)
@@ -74,48 +75,52 @@ multi_naive_bayes = MultinomialNB().fit(x_train, y_train)
 bernoulli_naive_bayes = BernoulliNB().fit(x_train, y_train)
 
 
-print("bayes")
+print("Bayes")
 print(gaussian_naive_bayes_fit.score(x_test, y_test))
 print(multi_naive_bayes.score(x_test, y_test))
 print(bernoulli_naive_bayes.score(x_test, y_test))
-print(multi_naive_bayes.predict_proba(x_test[:10]))
+
 
 
 #seems good, gives changing results
-#clf = OneVsRestClassifier(LinearSVC(class_weight=class_weight))
-#clf_fit = clf.fit(x_train, y_train).score(x_test, y_test)
+#one_vs_rest = OneVsRestClassifier(estimator=).fit(x_train, y_train).score(x_test, y_test)
 #print("One vs. Rest")
-#print(clf_fit)
+#print(one_vs_rest)
 
-#gradient_boosting = GradientBoostingClassifier()
-#gradient_boosting_fit = GradientBoostingClassifier.fit(x_train,y_train)
-#print("Gradient Boosting")
-#print(gradient_boosting_fit.score(x_test, y_test))
+gradient_boosting = GradientBoostingClassifier()
+gradient_boosting_fit = GradientBoostingClassifier().fit(x_train,y_train)
+print("Gradient Boosting")
+print(gradient_boosting_fit.score(x_test, y_test))
+
+ada_classifier = AdaBoostClassifier().fit(x_train, y_train)
+ada_classifier_score = ada_classifier.score(x_test, y_test)
+print("AdaBoost")
+print(ada_classifier_score)
 
 # seems good, gives constant result, best atm
-# = LogisticRegression()
-#logistic_regression_fit = logistic_regression.fit(x_train,y_train)
-#print("logisticregr")
-#print(logistic_regression_fit.score(x_test, y_test))
+logistic_regression = LogisticRegression(class_weight=class_weight)
+logistic_regression_fit = logistic_regression.fit(x_train,y_train)
+print("Logistic Regression")
+print(logistic_regression_fit.score(x_test, y_test))
 
 # seems good, gives close to constant result
 decision_tree = DecisionTreeClassifier(class_weight=class_weight)
 decision_tree_fit = decision_tree.fit(x_train, y_train)
 decision_tree_prediction = decision_tree.predict(x_test)
 print(confusion_matrix(y_test, decision_tree_prediction))
-print("decisiontree")
+print("Decision Tree")
 print(decision_tree_fit.score(x_test, y_test))
 
 # seems good, gives changing results
 linear_svc = svm.LinearSVC(class_weight=class_weight)
 linear_svc_fit = linear_svc.fit(x_train, y_train)
-print("linearsvc")
+print("Linear SVC")
 print(linear_svc_fit.score(x_test, y_test))
 
 # seems good
 random_forest = RandomForestClassifier(class_weight=class_weight)
 random_forest_fit = random_forest.fit(x_train, y_train)
-print("randomforest")
+print("Random Forest")
 print(random_forest_fit.score(x_test, y_test))
 print(random_forest.predict(x_test))
 
@@ -124,10 +129,8 @@ mlp = MLPClassifier()
 print("mlp")
 print(mlp.fit(x_train, y_train).score(x_test, y_test))
 mlp = MLPClassifier()
-print("mlp")
 print(mlp.fit(x_train, y_train).score(x_test, y_test))
 mlp = MLPClassifier()
-print("mlp")
 print(mlp.fit(x_train, y_train).score(x_test, y_test))
 
 
@@ -138,20 +141,19 @@ print(mlp.fit(x_train, y_train).score(x_test, y_test))
 
 # Algorithm to calculate if there is profit to be made
 ####
-array = decision_tree.predict_proba(x_test[:3000])
-odds = odds[16000:19000]
+calcodds = logistic_regression_fit.predict_proba(x_test[:3000])
+odds = []
+for i in range(3000):
+    odds.append(x_test[i][-3:])
 result = y_test[:3000]
-calcodds = []
 
-for i in range(len(array)):
-    calcodds.append(list(array[i]))
 matches = 0
 profit = 0
 wrong = 0
 correct = 0
 for i in range(len(calcodds)):
     for j in range(len(calcodds[i])):
-        if 1.03 < calcodds[i][j]*odds[i][j] < 1.15 and j == 0:
+        if 1.1 < calcodds[i][j]*odds[i][j]:
             matches += 1
 
             if j == result[i]:
